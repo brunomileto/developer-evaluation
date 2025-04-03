0f765c97bf97551ed6d5dca68fce5f1f2b10ba74
Bruno Mileto <brunomileto@outlook.com>
2025-04-03 02:04:56 -0300

    fix: correct update database command in sh and ps1 scripts for proper execution

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_PATH="src/Ambev.DeveloperEvaluation.WebApi/Ambev.DeveloperEvaluation.WebApi.csproj"
+
+STARTUP_PROJECT="src/Ambev.DeveloperEvaluation.WebApi/Ambev.DeveloperEvaluation.WebApi.csproj"
+PROJECT_PATH="src/Ambev.DeveloperEvaluation.ORM/Ambev.DeveloperEvaluation.ORM.csproj"
 
 echo "Script directory: $DIR"
 
@@ -8,7 +10,7 @@ echo "Applying Migrations..."
 
 dotnet ef database update \
   --project "$DIR/../$PROJECT_PATH" \
-  --startup-project "$DIR/../$PROJECT_PATH"
+  --startup-project "$DIR/../$STARTUP_PROJECT"
 
 
 if [ $? -eq 0 ]; then
@@ -17,3 +19,4 @@ else
     echo "Erro ao aplicar as migrations."
     exit 1
 fi
+ 
