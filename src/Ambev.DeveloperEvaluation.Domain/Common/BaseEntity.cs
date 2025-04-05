8f47e93c5b4bc7d006265119816110ec91d3090f
Bruno Mileto <brunomileto@outlook.com>
2025-04-05 20:18:27 -0300

    feat: add domain event support to base Entity class

--- src/Ambev.DeveloperEvaluation.Domain/Common/BaseEntity.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Common/BaseEntity.cs
@@ -20,4 +20,19 @@ public class BaseEntity : IComparable<BaseEntity>
 
         return other!.Id.CompareTo(Id);
     }
+    
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
 }
